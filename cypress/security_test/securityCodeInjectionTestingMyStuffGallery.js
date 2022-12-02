15435e00fd7ca35e5242b0a77343c7100ea8b576
mcincovshred <maryann.c@vshred.com>
2022-12-03 04:44:40 +0800

    Security Cod Injection Testing
    Security Test Cases for Vshred My Stuff Gallery page

---
+++ cypress/security_test/securityCodeInjectionTestingMyStuffGallery.js
@@ -0,0 +1,92 @@
+/// <reference types="Cypress" />
+import 'cypress-file-upload'
+
+describe('Code Injection Test', ()=>{
+
+    it.only('Security Code Injection Testing My Stuff Profile Page', () => 
+    {
+
+        const scriptImagePath = '<script>alert(\'test\')<:script>.png';
+        const dayjs = require('dayjs')
+        const Timenow24hoursa = dayjs().format("Hmmss");
+        const CAUser = 'CA_SuperAdmin_'+dayjs().format('DDMMYYYY')+Timenow24hoursa;
+        cy.create_user_as_super_admin(CAUser)
+        cy.login(CAUser+'@catest.com','pass1234')
+        cy.get('.modal-active').click(-50, -50, { force: true });
+        // Update Profile Name
+        cy.get('#name').clear()
+        cy.get('#name').type("<script>alert('test')</script>")
+        cy.get('#profile-gender').select("Male")
+        cy.get('.row > .col-md-3 > .btn').click()
+        cy.get('.toast-message').invoke("text").should("eq","Profile updated")
+
+
+        //cy.logout()
+    })
+
+    it.skip('profile Page Code Injection not allowed', () => 
+    {
+        const scriptImagePath = '<script>alert(\'test\')<:script>.png';
+        cy.visit('https://testing-06.vshred.com')
+        cy.login('mc_member@test.com','pass1234')
+        cy.get('.btn').click()
+        cy.get('.menu-vertical > :nth-child(4) > a').click()
+        cy.get('.btn__text').click()
+        cy.get('#images').attachFile(scriptImagePath)
+        cy.wait(5000)
+        
+        //cy.get('#images').a
+        //(scriptImagePath)
+        //cy.logout()
+    })
+
+    it.skip('profile Page Code Injection not allowed', () => 
+    {
+        const scriptImagePath = '<script>alert(\'test\')<:script>.png';
+        cy.visit_vshred_test_env()
+        cy.login('mc_member@test.com','pass1234')
+        cy.get('.btn').click()
+        cy.get('.menu-vertical > :nth-child(4) > a').click()
+        cy.get('.btn__text').click()
+        cy.get('#images').attachFile(scriptImagePath)
+        cy.wait(5000)
+        
+        //cy.get('#images').a
+        //(scriptImagePath)
+        //cy.logout()
+    })
+
+    it.skip('My Stuff Gallery Image Upload Code Injection not allowed', () => 
+    {
+        const scriptImagePath = '<script>alert(\'test\')<:script>.png';
+        const dayjs = require('dayjs')
+        const Timenow24hoursa = dayjs().format("Hmmss");
+        const CAUser = 'CA_SuperAdmin_'+dayjs().format('DDMMYYYY')+Timenow24hoursa;
+        cy.create_user_as_super_admin(CAUser)
+        cy.login(CAUser+'@catest.com','pass1234')
+        cy.get('.menu-vertical > :nth-child(4) > a').click()
+        cy.get('.btn__text').click()
+        //upload image
+        cy.get('#images').attachFile(scriptImagePath)
+        cy.get('.toast-message').invoke("text").should("eq","Added 1 images to your gallery!")
+        cy.get('.modal-active').click(-50, -50, { force: true });
+        cy.wait(5000)
+        //update image title
+        cy.get('.edit-image > .fa').click()
+        cy.get('#swal2-content > #edit-image-form > .row > :nth-child(2) > #title2').type(scriptImagePath)
+        cy.get('.swal2-confirm').click()
+        cy.get('.toast-message').should('contain',"Updated image")
+        //update image desc
+        cy.wait(5000)
+        cy.get('.edit-image > .fa').click()
+        cy.get('#swal2-content > #edit-image-form > .row > :nth-child(2) > #title2')
+        cy.get('.swal2-confirm').click()
+        cy.get('.toast-message').should('contain',"Updated image") 
+        //delete image
+        cy.get('.delete-image > .fa').click()
+        cy.get('.swal2-confirm').click()
+        cy.get('.toast-message').invoke("text").should("eq","Deleted image: " + scriptImagePath)
+        
+    })
+    
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,64 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import 'cypress-file-upload'
+import './commands'
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from failing the test
+  return false
+})
+
+Cypress.Commands.add('login',(email,password) =>{
+    cy.get('.btn__text').click()
+    cy.get('#email').type(email)
+    cy.get('#password').type(password)
+    cy.get(':nth-child(3) > .btn').click()
+})
+
+Cypress.Commands.add('logout',() => {
+    cy.get(':nth-child(8) > a').click()
+    cy.get('.menu-vertical > :nth-child(5) > a')
+})
+
+Cypress.Commands.add('visit_vshred_test_env',() => {
+    cy.visit('https://testing-06.vshred.com')
+    cy.clearCookies()
+})
+
+Cypress.Commands.add('create_user_as_super_admin', (CAUser) => 
+    {   
+        cy.visit_vshred_test_env()
+        cy.login('super@example.com','asdfasdf')
+        cy.get(':nth-child(14) > .nav-link').click()
+        cy.get('.btn > span').click()
+        cy.get('div > #user-name').type(CAUser)
+        cy.get('div > #user-email').type(CAUser+'@catest.com')
+        cy.get('div > #user-password').type('pass1234')
+        cy.get('div > #user-confirm').type('pass1234')
+        cy.get('#createFormModal___BV_modal_footer_ > .btn-primary').click()
+        cy.wait(5000)
+        cy.get('.navbar > .nav > :nth-child(2) > .nav-link').click()
+    })
