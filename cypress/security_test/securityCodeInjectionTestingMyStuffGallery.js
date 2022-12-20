1ed98c0dc6afb79389ad3c68a12f4b8b429e29b2
mcincovshred <maryann.c@vshred.com>
2022-12-20 23:14:50 +0800

    Security Code Injection Testing My Stuff Profile Page
    added TC Security Code Injection Testing My Stuff Profile Page

--- cypress/security_test/securityCodeInjectionTestingMyStuffGallery.js
+++ cypress/security_test/securityCodeInjectionTestingMyStuffGallery.js
@@ -3,7 +3,7 @@ import 'cypress-file-upload'
 
 describe('Code Injection Test', ()=>{
 
-    it.skip('Security Code Injection Testing My Stuff Profile Page', () => 
+    it.only('Security Code Injection Testing My Stuff Profile Page', () => 
     {
 
         const scriptImagePath = '<script>alert(\'test\')<:script>.png';
@@ -24,7 +24,7 @@ describe('Code Injection Test', ()=>{
         cy.logout()
     })
 
-    it.skip('My Stuff Gallery Image Upload Code Injection not allowed', () => 
+    it.only('My Stuff Gallery Image Upload Code Injection not allowed', () => 
     {
         const scriptImagePath = '<script>alert(\'test\')<:script>.png';
         const dayjs = require('dayjs')
@@ -57,10 +57,13 @@ describe('Code Injection Test', ()=>{
         cy.get('.delete-image > .fa').click()
         cy.get('.swal2-confirm').click()
         cy.get('.toast-message').should('contain',"Deleted image")
+        //cy.get('.toast-message').should('not.include.text', 'test')
+        cy.wait(5000)
+        cy.get('.menu-vertical > :nth-child(5) > a').click()
         
     })
 
-    it.skip('Security Code Injection Testing My Stuff Profile Page', () => 
+    it.only('Security Code Injection Testing My Stuff Profile Page', () => 
     {
 
         const scriptImagePath = '<script>alert(\'test\')<:script>.png';
@@ -79,26 +82,45 @@ describe('Code Injection Test', ()=>{
         cy.get('.form-horizontal > .col-md-3 > .btn').click()
         cy.get('.toast-message').should('contain',"Password updated") 
         cy.get('.modal-active').click(-50, -50, { force: true });
-        //cy.get('.menu-vertical > :nth-child(5) > a').click()
+        cy.get('.menu-vertical > :nth-child(5) > a').click()
         //cy.logout()
     })
 
-    it.only('Security Code Injection Testing My Stuff Profile Page', () => 
+    it.only('Security Code Injection Testing Vshred Boost Questionaire', () => 
     {
 
         const scriptImagePath = '<script>alert(\'test\')<:script>.png';
         const dayjs = require('dayjs')
         const Timenow24hoursa = dayjs().format("Hmmss");
         const CAUser = 'CA_SuperAdmin_'+dayjs().format('DDMMYYYY')+Timenow24hoursa;
+        
         cy.create_user_as_super_admin(CAUser)
         cy.login(CAUser+'@catest.com','pass1234')
         cy.wait(5000)
-        cy.get('.modal-active').click(-50, -50, { force: true });
+        cy.get('.modal-active').click(-50, -50, { force: true })
         // Update Password
         cy.get('#questionnaire').click()
-        cy.get(':nth-child(2) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type('Test')
-        cy.get(':nth-child(4) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type('01')
-        //cy.get('.menu-vertical > :nth-child(5) > a').click()
+        cy.get(':nth-child(2) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type(scriptImagePath)
+        cy.get(':nth-child(4) > [data-v-e264feca=""] > .cdp-field > .cdp-input').invoke('removeAttr','type').type('1980-12-12{enter}');
+        cy.get(':nth-child(5) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type(scriptImagePath)
+        cy.get(':nth-child(6) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type(scriptImagePath)
+        cy.get(':nth-child(7) > [data-v-e264feca=""] > .cdp-field > .cdp-input').select(3)
+        cy.get(':nth-child(8) > [data-v-e264feca=""] > .cdp-field > .cdp-input').select(3)
+        cy.get(':nth-child(9) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type('Test')
+        cy.get(':nth-child(10) > [data-v-e264feca=""] > :nth-child(1) > :nth-child(2) > .radio-container > .checkmark').click()
+        cy.get(':nth-child(11) > [data-v-e264feca=""] > :nth-child(1) > :nth-child(2) > .radio-container > .checkmark').click()
+        cy.get(':nth-child(12) > [data-v-e264feca=""] > :nth-child(1) > :nth-child(2) > .radio-container > .radio-container-label').click()
+        cy.get(':nth-child(12) > [data-v-e264feca=""] > :nth-child(1) > :nth-child(2) > .radio-container > .radio-container-label').click()
+        cy.get(':nth-child(2) > :nth-child(1) > :nth-child(2) > .radio-container > .radio-container-label').click()
+        cy.get(':nth-child(14) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type(scriptImagePath)
+        cy.get(':nth-child(15) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type(scriptImagePath)
+        cy.get(':nth-child(16) > [data-v-e264feca=""] > .cdp-field > .cdp-input').type(scriptImagePath)
+        cy.get('#recaptcha_daf684c020cdc47e9b4d21b4163e7ca0').click()
+        cy.get('[data-modal-index="1"] > .modal-content > .boxed').should('contain',"Vshred Boost Questionnaire Submitted!") 
+        cy.wait(5000)
+        cy.get('[data-modal-index="1"]').click(-50, -50, { force: true })
+        cy.wait(5000)
+        cy.get('.menu-vertical > :nth-child(5) > a').click()
         //cy.logout()
     })
     
